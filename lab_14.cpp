dd4389ea9fc1fb93da2810aefc47695c31c5670d
Tanmayee Chalamalasetti <tanmayeesc@MacBookPro.attlocal.net>
2025-09-28 00:32:31 -0700

    Push 2

--- lab_14.cpp
+++ lab_14.cpp
@@ -7,14 +7,17 @@ class Color {
   int red;
   int green;
   int blue;
+  string name;
 
  public:
   int getRed() { return red; }
   void setRed(int r) { red = r; }
-  int getBlue() { return green; }
-  void setBlue(int g) { green = g; }
+  int getGreen() { return green; }
+  void setGreen(int g) { green = g; }
   int getBlue() { return blue; }
   void setBlue(int b) { blue = b; }
+  string getName() { return name; }
+  void setName(string n) { name = n; }
 };
 
 void print(Color);
@@ -23,6 +26,24 @@ int main() {
   cout << fixed << setprecision(2);
   // I lookd up RGB values of colors I like
   // Color 1
+  Color burgundy;
+  burgundy.setName("Burgundy");
+  burgundy.setRed(128);
+  burgundy.setGreen(0);
+  burgundy.setBlue(32);
+  // burgundy.print(); //does this work?
+  print(burgundy);
+
+  Color viridian;
+  v.setName("Burgundy");
+  viridian.setRed(64);
+  viridian.setGreen(130);
+  viridian.setBlue(109);
+  // viridian.print(); //does this work?
+  print(viridian);
 
   return 0;
 }
+
+void print(Color s){cout << "(" << s.getRed() << s.getGreen() << ", "
+                         << s.getBlue() << " )"}
